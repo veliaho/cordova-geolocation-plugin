b7b69f7b7324ed7bee4cf538321a3a91bcaaf654
Janne Kuuskeri <janne.kuuskeri@gmail.com>
2016-10-18 19:14:06 +0300

    Added missing files

---
+++ www/geolocation.js
@@ -0,0 +1,3 @@
+var serviceName = 'com.vinka.cordova.plugin.geolocation.GeolocationService';
+var factory = require('com.red_folder.phonegap.plugin.backgroundservice.BackgroundService');
+module.exports = factory.create(serviceName);
