83847167ad5cfdbe173d14b4cf3c56469ea8139c
Janne Kuuskeri <janne.kuuskeri@gmail.com>
2016-10-19 09:30:20 +0300

    Sending geopos to JS every time location changes

--- src/android/GeolocationService.java
+++ src/android/GeolocationService.java
@@ -16,59 +16,68 @@ import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
 
 public class GeolocationService extends BackgroundService {
 
-  private class ViperLocationListener implements LocationListener {
-    private final static String TAG = "VIPER_LOCATION";
+    private class ViperLocationListener implements LocationListener {
+        private final static String TAG = "VIPER_LOCATION";
 
-    @Override
-    public void onLocationChanged(Location loc) {
-      Log.d(TAG, "===========================================================================");
-      Log.d(TAG, "Location changed : Lat: " + loc.getLatitude()+ " Lng: " + loc.getLongitude());
-      lastLocation = loc;
+        @Override
+        public void onLocationChanged(Location loc) {
+            Log.d(TAG, "===========================================================================");
+            Log.d(TAG, "Location changed : Lat: " + loc.getLatitude() + " Lng: " + loc.getLongitude());
+            lastLocation = loc;
+            GeolocationService.this.handleLastResult(this.formatGeopos(loc));
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
     }
 
-    @Override
-    public void onProviderDisabled(String provider) {}
-
-    @Override
-    public void onProviderEnabled(String provider) {}
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {}
-  }
-
-  private Location lastLocation = null;
-  private LocationManager locationManager = null;
-  private LocationListener locationListener = null;
+    private Location lastLocation = null;
+    private LocationManager locationManager = null;
+    private LocationListener locationListener = null;
 
     private final static String TAG = GeolocationService.class.getSimpleName();
 
     private String mHelloTo = "World";
 
-  @Override
-  public void onCreate() {
-    locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-    locationListener = new ViperLocationListener();
-    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-  }
+    @Override
+    public void onCreate() {
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        locationListener = new ViperLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
+
+    private JSONObject formatGeopos(Location loc) {
+        JSONObject result = new JSONObject();
+
+        result.put("accuracy", loc.getAccuracy());
+        result.put("bearing", loc.getBearing());
+        result.put("lat", loc.getLatitude());
+        result.put("lng", loc.getLongitude());
+        result.put("provider", loc.getProvider());
+        result.put("speed", loc.getSpeed());
+        result.put("time", loc.getTime());
+        result.put("lng", log.getLongitude());
+
+        return result;
+    }
 
     @Override
     protected JSONObject doWork() {
         JSONObject result = new JSONObject();
 
         try {
-      if (this.lastLocation != null) {
-        result.put("accuracy", this.lastLocation.getAccuracy());
-        result.put("bearing", this.lastLocation.getBearing());
-        result.put("lat", this.lastLocation.getLatitude());
-        result.put("lng", this.lastLocation.getLongitude());
-        result.put("provider", this.lastLocation.getProvider());
-        result.put("speed", this.lastLocation.getSpeed());
-        result.put("time", this.lastLocation.getTime());
-        result.put("lng", this.lastLocation.getLongitude());
-      } else {
-        Log.d(TAG, "/////// doWork() does not have location");
-      }
+            if (this.lastLocation != null) {
+                result = this.formatGeopos(this.lastLocation);
+            } else {
+                Log.d(TAG, "/////// doWork() does not have location");
+            }
         } catch (JSONException e) {
         }
 
